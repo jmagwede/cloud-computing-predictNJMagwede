acccc2a9fd108c6267b7027e695d220882bac817
jmagwede <147488078+jmagwede@users.noreply.github.com>
2023-11-02 13:03:27 +0200

    Update write_data_to_dynamodb.py

--- student_solution_files/write_data_to_dynamodb.py
+++ student_solution_files/write_data_to_dynamodb.py
@@ -34,28 +34,28 @@ def lambda_handler(event, context):
     # on a unique value to prevent errors when writing to DynamoDB. **
     
     # --- Insert your code here ---
-    rid = None # <--- Replace this value with your code.
+    rid = random.randint (1, 1000000000) # <--- Replace this value with your code.
     # -----------------------------
     
     # ** Instantiate the DynamoDB service with the help of the boto3 library **
     
     # --- Insert your code here ---
-    dynamodb = None # <--- Replace this value with your code.
+    dynamodb = boto3.resource('dynamodb') # <--- Replace this value with your code.
     # -----------------------------
     
     # Instantiate the table. Remember pass the name of the DynamoDB table created in step 4
-    table = dynamodb.Table('# Insert the name of your generated DynamoDB table here')
+    table = dynamodb.Table('# Judy-portfolio-data-table')
     
     # ** Write the responses to the table using the put_item method. **
 
     # Complete the below code so that the appropriate 
     # incoming data is sent to the matching column in your DynamoDB table
     # --- Insert your code here ---
-    db_response = table.put_item(Item={'ResponsesID': None, # <--- Insert the correct variable
-                        'Name': None, # <--- Insert the correct variable
-                        'Email': None, # <--- Insert the correct variable
-                        'Cell': None, # <--- Insert the correct variable
-                        'Message': None # <--- Insert the correct variable
+    db_response = table.put_item(Item={'ResponsesID': 100, # <--- Insert the correct variable
+                        'Name': Student, # <--- Insert the correct variable
+                        'Email': student@explore-ai.net, # <--- Insert the correct variable
+                        'Cell': 123456789, # <--- Insert the correct variable
+                        'Message': Empty Message # <--- Insert the correct variable
     })
     # -----------------------------
 
